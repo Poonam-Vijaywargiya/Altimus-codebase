d13720e6ff7fa2c750d8154dcba8a45e8e25ff31
Poonam Vijaywargiya <poonamvijaywargiya@Poonams-MacBook-Pro.local>
2023-09-09 22:10:26 +0530

    heroku commit

--- Server/index.js
+++ Server/index.js
@@ -32,7 +32,7 @@ const transporter = nodemailer.createTransport({
 
 app.get('/', function(req, res){
     res.sendFile(
-        path.join(__dirname, "../Client/build/index.html"), 
+        path.join(__dirname, "../Client/dist/index.html"), 
         function(err) {
             if(err) {
                 res.status(500).send(err)
