56362e7bb946eec587e4bd6609bf2533a3f99140
Poonam Vijaywargiya <poonamvijaywargiya@Poonams-MacBook-Pro.local>
2023-09-17 21:08:49 +0530

    heroku commit

--- Server/Client/src/Components/Contact/Contact.jsx
+++ Server/Client/src/Components/Contact/Contact.jsx
@@ -9,29 +9,29 @@ const Contact = () => {
   return (
     <div className='contact-container'>
     <Header/>
-    <div style={{display:'flex', alignItems: 'center'}}>
-    <div className='contact-card'>
-      <Typography variant="h4" gutterBottom>
-        Contact Us
-      </Typography>
-      <Typography variant="body1" paragraph>
-              If you need assistance, please feel free to contact us.
-      </Typography>
-      <Grid >
-        <Grid item xs={12} sm={6}>
-          <div className="contact-info">
-            <EmailIcon className="contact-icon" color="primary" />
-            <Typography variant="body1">
-              <strong>Email:</strong> 
-              <a href="mailto:admin@altimus.energy" style={{textDecoration: 'none'}}> admin@altimus.energy</a>
-            </Typography>
-          </div>
+    <div className ='contact-main-container'>
+      <div className='contact-card'>
+        <Typography variant="h4" gutterBottom>
+          Contact Us
+        </Typography>
+        <Typography variant="body1" paragraph>
+                If you need assistance, please feel free to contact us.
+        </Typography>
+        <Grid >
+          <Grid item xs={12} sm={6}>
+            <div className="contact-info">
+              <EmailIcon className="contact-icon" color="primary" />
+              <Typography variant="body1">
+                <strong>Email:</strong> 
+                <a href="mailto:admin@altimus.energy" style={{textDecoration: 'none'}}> admin@altimus.energy</a>
+              </Typography>
+            </div>
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
-    <div style={{flex: 2  }}>
-      <img src={contactus} style={{height: '50%', width: '90%'}}/>
-    </div>
+      </div>
+      <div style={{flex: 2 }}>
+        <img src={contactus} className='contact-img' />
+      </div>
     </div>
     </div>
   )
